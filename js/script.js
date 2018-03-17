e51a5a8caa31d09080e8cd886db102eb3d0049b6
Amriesgrace <obieri.grace@gmail.com>
2018-03-17 15:18:40 +0100

    50% homepage

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-$(document).ready(function(){
-	$.widget( "custom.iconselectmenu", $.ui.selectmenu, {
-		_renderItem: function( ul, item ) {
-          	var li = $( "<li>" ),
-            	wrapper = $( "<div>", { text: item.label } );
-	   
-          	$( "<span>", {
-	            style: item.element.attr( "data-style" ),
-	            "class": "ui-icon " + item.element.attr( "data-class" )
-          	})
-            .appendTo( wrapper );
-	   
-          	return li.append( wrapper ).appendTo( ul );
-        }
-	});
+$(document).ready(function() {
+    $(".testimonial-carousel").slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 6000,
+        prevArrow: $(".testimonial-carousel-controls .prev"),
+        nextArrow: $(".testimonial-carousel-controls .next")
+    });
+    /*$(function() {
+        var BV = new $.BigVideo({container: $('#video_bg')});
 
-	$( "#country_code" )
-    .iconselectmenu()
-    .iconselectmenu( "menuWidget" )
-      .addClass( "ui-menu-icons customicons" );
+        BV.init();
+        BV.getPlayer().pause();
 
+        BV.show('video/SAMS_Advert.mp4', {doLoop:true}, {autoplay:true});
+
+    });*/
+    $(".features_card").mouseover( function (e) {
+        // body...
+        console.log(e.target.value);
+        $(this).find(".feature_details").show();
+         $(this).toggleClass('flip');
+    })
+    $(".features_card").mouseleave( function (e) {
+        // body...
+        console.log(e.target.value);
+        $(this).find(".feature_details").hide();
+    })
+    // var card = document.querySelector('.features_card');
+    // function flipCard(e){
+    //     console.log(e.target);
+    //     (this).find(".feature_detaiils").show();
+    // }
+    // card.addEventListener('mouseover', flipCard);
 });
